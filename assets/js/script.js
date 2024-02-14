16e2bb88893a770942cbe25d3fc7025767b2d993
laura fox <laurafox91@gmail.com>
2024-02-14 19:49:55 +0000

    Add multiplication game

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ let num2 = Math.floor(Math.random() * 25);
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
 } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -66,6 +68,9 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"]
+
     } else {
         alert(`Unimplemented operator $(operator)`);
         throw `Unimplemented operator $(operator). Aborting!`;
@@ -102,6 +107,9 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
     
 }
