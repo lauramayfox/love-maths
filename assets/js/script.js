c436444b7c6826b3cdc523db5a2a2d6aba90a1e0
laura fox <laurafox91@gmail.com>
2024-02-14 20:04:51 +0000

    Add the subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,18 +22,21 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
-    //Creates 2 random numbers between 1 and 25
-let num1 = Math.floor(Math.random() * 25);
-let num2 = Math.floor(Math.random() * 25);
 
-if (gameType === "addition") {
-    displayAdditionQuestion(num1, num2);
-} else if (gameType === "multiply") {
-    displayMultiplyQuestion(num1, num2);
-} else {
-    alert(`Unknown game type: ${gameType}`);
-    throw `Unknown game type: ${gameType}. Aborting!`;
-}
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract" ) {
+        displaySubtractQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 /** 
@@ -71,6 +74,8 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]
 
+    } else if (operator === "-") {
+        return [operand1 - operand2,"subtract"];
     } else {
         alert(`Unimplemented operator $(operator)`);
         throw `Unimplemented operator $(operator). Aborting!`;
@@ -103,7 +108,10 @@ function displayAdditionQuestion (operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+document.getElementById('operator').textContent = '-';
 
 }
 
